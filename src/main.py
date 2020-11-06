8f2c6acaa8d2815873a88fdd7bc74fcd4fd5111a
MatOgr <matjonne@gmail.com>
2020-11-06 05:48:25 +0100

    Keep it this way

--- src/main.py
+++ src/main.py
@@ -1,15 +1,23 @@
 import os
+
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.image import AsyncImage
 from kivy.uix.popup import Popup
 from kivymd.app import MDApp
 from kivy.lang.builder import Builder
-from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.screenmanager import Screen
 from src.bcknd import Loader
 from kivy.uix.widget import Widget
 from kivy.utils import platform
-from kivyauth.facebook_auth import initialize_fb, login_facebook, logout_facebook
-from kivyauth.providers import login_providers
+
+
+class MyLoader(Widget):
+    url = 'https://moodup.team/test/info.php'
+    loader = Loader(url)
+    if platform == 'android':
+        loader.SAVE_FOLDER = os.getenv('DIRECTORY_PICTURES')
+
+    def show_recipe(self):
+        self.recipe.text = str(self.loader.site_data['description'])
 
 
 class MenuScreen(Screen):
@@ -32,7 +40,6 @@ class MoodApp(MDApp):
     data = {
         'campfire': 'Get the recipe',
         'facebook': 'Zaloguj przez Facebooka'}
-    current_provider = ''
 
     def __init__(self, **kwargs):
         self.title = "MoodApp"
@@ -40,7 +47,6 @@ class MoodApp(MDApp):
         super().__init__(**kwargs)
 
     def build(self):
-        initialize_fb(self.after_login, self.cancel_listener, self.error_listener)
         screen = Builder.load_file('main.kv')
         return screen
 
@@ -51,46 +57,16 @@ class MoodApp(MDApp):
     def show_popup(self, nr):
         show = P()
         P.id = nr
-        popupWindow = Popup(title='Zapisywanie obrazu %d' % nr,
-                            content=show,
-                            size_hint=(0.6, 0.4))
-        popupWindow.open()
+        popup_window = Popup(title='Zapisywanie obrazu %d' % nr,
+                             content=show,
+                             size_hint=(0.6, 0.4))
+        popup_window.open()
 
     def call(self, instance):
         if instance.icon == 'campfire':
             self.root.current = 'second'
             instance.parent.close_stack()
             self.root.transition.direction = 'left'
-        elif instance.icon == 'facebook':
-            self.fb_login()
-
-    # secret departament
-    def fb_login(self, *args):
-        login_facebook()
-        self.current_provider = login_providers.facebook
-
-    def after_login(self, name, email, photo_uri):
-        self.root.current = 'homescreen'
-        self.update_ui(name, email, photo_uri)
-
-    def after_logout(self):
-        self.update_ui('', '', '')
-        self.root.current = 'loginscreen'
-
-    def update_ui(self, name, email, photo_uri):
-        print("Welcome, {}".format(name))
-        print("Your Email: {}".format(email) if email else "Your Email: Could not fetch email")
-
-    def logout_(self):
-        # logout(current_provider, self.after_logout)
-        logout_facebook(self.after_logout)
-
-    def cancel_listener(self):
-        print("Login cancelled")
-
-    def error_listener(self):
-        print("Error logging in.")
-
 
 
 MoodApp().run()
